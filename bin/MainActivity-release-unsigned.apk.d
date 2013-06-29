f3f1c8103bf870cd3c2b8abf499658c9ac047929
20twenty <brady_benware@hotmail.com>
2013-06-29 13:48:04 -0700

    update

---
+++ bin/MainActivity-release-unsigned.apk.d
@@ -0,0 +1,4 @@
+/home/bbenware/projects/OtherVenues/bin/MainActivity-release-unsigned.apk : \
+/home/bbenware/projects/OtherVenues/bin/MainActivity.ap_ \
+/home/bbenware/projects/OtherVenues/bin/classes.dex \
+/home/bbenware/projects/OtherVenues/libs/android-support-v4.jar \
