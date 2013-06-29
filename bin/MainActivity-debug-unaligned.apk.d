db17ae8fe2d31085ffe13e4ed3c80a85bc290bde
20twenty <brady_benware@hotmail.com>
2013-06-29 13:30:40 -0700

    updated build process

--- bin/MainActivity-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/home/bbenware/projects/OtherVenues/bin/MainActivity-debug-unaligned.apk : \
-/home/bbenware/projects/OtherVenues/bin/MainActivity.ap_ \
-/home/bbenware/projects/OtherVenues/bin/classes.dex \
-/home/bbenware/projects/OtherVenues/libs/android-support-v4.jar \
